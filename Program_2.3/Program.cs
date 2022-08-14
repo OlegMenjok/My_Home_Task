b45d8d3af8bc50ecde0e802fc03e9bdfc74dd5f4
OFFICE\oleh.menok <oleh.menok@pm.bet>
2022-08-14 10:02:50 +0300

    pr

--- Program_2.3/Program.cs
+++ Program_2.3/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int indexMax = 0;
+Console.Write("Enter quantity number of array:\t ");
+int userEntered = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+var nums = new int[userEntered];
+Console.WriteLine($"Enter any {userEntered} numbers: ");
+
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.WriteLine($"Enter {i+1} value: ");
+    nums[i] = int.Parse(Console.ReadLine());
+}
+
+double minValue = nums[0];
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] < minValue)
+    {
+        minValue = nums[i];
+    }
+}
+Console.WriteLine("Min elements is: " + minValue);
+
+double maxValue = nums[0];
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] > maxValue)
+    {
+        maxValue = nums[i];
+    }
+}
+Console.WriteLine("Max elements is: " + maxValue);
+
+var sum = 0;
+for (var i = 0; i < nums.Length; i++)
+{
+    sum = sum + nums[i];
+}
+Console.WriteLine("Sum of elements is: " + sum);
+
+var avsum = 0;
+for (var i = 0; i < nums.Length; i++)
+{
+    avsum += nums[i]/2;
+}
+Console.WriteLine("AvSum of elements is: " + avsum);
+
+
+
+
+
